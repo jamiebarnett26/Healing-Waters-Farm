e33e1ade0188b14d46fe4525a4890765e9a2c73b
Nicole <nicole.tacconi@princeton.edu>
2024-11-03 17:08:03 -0500

    PWA edits: no errors w manifest and sw

--- serviceWorker.js
+++ serviceWorker.js
@@ -8,12 +8,13 @@
 const staticPhoneStore = "phone-store-site-v1"
 
 const assets = [
-  "/",
-  "/index.html"
+  "/index",
+  "/"
 ]
 
 // runs immediately after installation: sets up resources needed right away
 self.addEventListener("install", installEvent => {
+    console.log("WE ARE IN THE EVENTLISTENER!")
   installEvent.waitUntil(
     caches.open(staticPhoneStore).then(cache => {
       cache.addAll(assets)
