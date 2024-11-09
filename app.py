cf2b765d9af1048236b222f2d74a01dd80409ef5
David Beeson <120219264+DavyBee@users.noreply.github.com>
2024-11-09 13:23:23 -0500

    Added tasks to cards in crop task list homepage

--- app.py
+++ app.py
@@ -27,9 +27,29 @@ def index():
 
 #-----------------------------------------------------------------------
 
+def getCardInfo():
+    user_crops = database.get_user_crops(1)
+    user_crop_infos = []
+    all_todos = []
+    for user_crop in user_crops:
+        user_crop_infos.append(database.full_crop_info(user_crop['crop_info_id']))
+
+        todos = [
+            {"task": "Start indoor seeding", "date": user_crop['indoor_seed_starting_date'], "done": False},
+            {"task": "Transplant plants from indoor to outdoor", "date": user_crop['transplanting_date'], "done": False},
+            {"task": "Direct sowing", "date": user_crop['direct_sow_date'], "done": False},
+            {"task": "Prepare for harvest", "date": user_crop['harvest_date'], "done": False}
+        ]
+        all_todos.append(todos)
+    
+    crops_with_todos = zip(user_crop_infos, all_todos)
+    
+    return crops_with_todos
+
 @app.route('/homepage', methods = ["GET"])
 def goHomepage():
-    html_code = flask.render_template('homepage/homepage.html')
+    crops_with_todos = getCardInfo()
+    html_code = flask.render_template('homepage/homepage.html', crops_with_todos=crops_with_todos)
     response = flask.make_response(html_code)
     return response
 
