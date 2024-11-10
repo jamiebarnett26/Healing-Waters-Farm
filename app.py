b2e8dd1474d175ffdb50edd9117575eb7e0ebddf
David Beeson <120219264+DavyBee@users.noreply.github.com>
2024-11-10 16:28:35 -0500

    added button clicks to new pages

--- app.py
+++ app.py
@@ -47,6 +47,20 @@ def getCardInfo():
     return crops_with_todos
 
 #-----------------------------------------------------------------------
+
+def get_crop_varieties_and_latin(user_id):
+    user_crops = database.get_user_crops(1)
+    user_crop_infos = []
+    variety_names = []
+    latin_names = []
+    for user_crop in user_crops:
+        user_crop_infos.append(database.full_crop_info(user_crop['crop_info_id']))
+        variety_names.append(user_crop_infos['variety_name'])
+        latin_names.append(user_crop_infos['latin_name'])
+
+    return zip(variety_names, latin_names)
+
+#-----------------------------------------------------------------------
 # Request from index login button, directs to homepage.html
 @app.route('/homepage', methods = ["GET"])
 def goHomepage():
