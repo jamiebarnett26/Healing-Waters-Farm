4b0428d84de1d2a030d032e36567863aa7cbf540
Nicole <nicole.tacconi@princeton.edu>
2024-11-23 15:47:37 -0500

    commented out script.js because of errors

--- static/js/script.js
+++ static/js/script.js
@@ -1,15 +1,15 @@
 
 // Home-page button for adding new crops
-document.getElementById('navigateButton').addEventListener('click', function() {
-  window.location.href = '/selectFamily';
-});
+// document.getElementById('navigateButton').addEventListener('click', function() {
+//   window.location.href = '/selectFamily';
+// });
 
 // Redirect to login
-document.getElementById('logoutButton').addEventListener('click', function() {
-  window.location.href = '/logout'
-})
+// document.getElementById('logoutButton').addEventListener('click', function() {
+//   window.location.href = '/logout'
+// })
 
-document.getElementById('homepageButton').addEventListener('click', function() {
-  window.location.href = '/homepage';
-});
+// document.getElementById('homepageButton').addEventListener('click', function() {
+//   window.location.href = '/homepage';
+// });
 
