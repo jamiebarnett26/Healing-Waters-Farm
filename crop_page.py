4e610d6e0f29443f3fdbbc89fa24903c2fe27644
David Beeson <120219264+DavyBee@users.noreply.github.com>
2024-12-10 23:43:04 -0500

    fixed footer, working on adding new tasks

--- crop_page.py
+++ crop_page.py
@@ -7,9 +7,6 @@ import json
 import sys
 import datetime
 
-def get_current_time():
-    return time.asctime(time.localtime())
-
 # edit for flask route with 
 @app.route('/getTasks/<user_crop_id>', methods=['GET'])
 def get_tasks(user_crop_id):
@@ -22,8 +19,9 @@ def get_tasks(user_crop_id):
     if len(todos) == 0:
        todos.append({"task": "nothing", "date": "nothing", "done": "not done"})
 
-    todos = sorted(todos, key=lambda todo: todo['date'])
+    todos = sorted(todos, key=lambda todo: (todo['completed'], todo['date']))
     return flask.jsonify(todos)
+
 
 # Helper function, returns crop to do list for cards
 @app.route('/checked', methods=['POST'])
@@ -32,12 +30,8 @@ def checked():
     task_id = data.get('task_id')
     completed = data.get('completed')
 
-    print('Task before:', completed)
-
     database.checkbox(task_id, completed)
-
-    print('Task after:', completed)
-
+    
     return flask.jsonify({
         'success': True,
         'completed': completed  # Return the updated status of the task
